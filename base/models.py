315d20eb26656fda66a4700c6a95296665853af9
darthpedroo <pedrovillarino06@gmail.com>
2024-02-26 15:13:33 -0300

    first commit

---
+++ base/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+
+class Paragraph(models.Model):
+    articleId = models.ForeignKey(Article, on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+
+    
+
