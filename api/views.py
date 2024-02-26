f89bb6388be6fb4403941d125b6bfdf651f4d707
darthpedroo <pedrovillarino06@gmail.com>
2024-02-26 16:17:04 -0300

    My first approach to use a class with the views (Wrong)

--- api/views.py
+++ api/views.py
@@ -1,8 +1,33 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from base.models import *
+from .serializer import *
+
+
+class GetAll:
+    def __init__(self, model, modelSerializer):
+        self.model = model.objects.all()
+        self.modelSerializer = modelSerializer(self.model, many=True)
+    
+    def return_view(self):
+        return Response(self.modelSerializer.data)
 
 @api_view(["GET"])
 def get_all_articles(request):
-    return Response("Me gusta el pene")
+    articles = GetAll(Article,ArticleSerializer)
+    return articles.return_view()
 
+@api_view(["GET"])
+def get_all_paragraphs(request):
+    paragraphs = GetAll(Paragraph, ParagraphsSerializer)
+    return paragraphs.return_view()
+
+
+@api_view(["GET"])
+def get_all_paragraphs_from_article(request,article_pk):
+    
+    paragraphs = Paragraph.objects.filter(articleId_id=article_pk)
+    serializer = ParagraphsSerializer(paragraphs, many=True)
+    
+    return Response(serializer.data)
