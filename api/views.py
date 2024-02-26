3af9498f98441326df27ed120309428c6a3d6f29
darthpedroo <pedrovillarino06@gmail.com>
2024-02-26 20:22:51 -0300

    Fixed the classes and applied strategy pattern

--- api/views.py
+++ api/views.py
@@ -3,32 +3,40 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from base.models import *
 from .serializer import *
+from rest_framework import status
+from abc import ABC, abstractmethod
 
-class BaseView(APIView):
-    model = None
-    model_serializer = None
+class ModelBuilder():
+    def __init__(self,model,model_serializer):
+        self.model = model
+        self.model_serializer = model_serializer
 
-class GetAllAPIView(BaseView):
-    def get(self, request):
-        queryset = self.model.objects.all()
+
+class GetDataStrategy(ABC):
+    @abstractmethod
+    def get(self,request):
+        pass
+
+class GetAllData(ModelBuilder, GetDataStrategy):
+
+    def get(self,request):
+        queryset = self.model.objects.all() #self.model = la clase (digamos Article) de BaseView. self.model.model la 
         serializer = self.model_serializer(queryset, many=True)
         return Response(serializer.data)
-
-class GetDataFilteredByPk(BaseView):
+        
+class GetDataFilteredByPk(GetDataStrategy):
     def get(self,request,*args, **kwargs):
         queryset = self.model.objects.filter(articleId_id=self.kwargs['pk'])
         serializer = self.model_serializer(queryset, many=True)
         return Response(serializer.data)
 
-class GetAllArticlesView(GetAllAPIView):
+class ArticleView(APIView):
     model = Article
     model_serializer = ArticleSerializer
 
-class GetAllParagraphsView(GetAllAPIView):
-    model = Paragraph
-    model_serializer = ParagraphsSerializer
+    get_strategy = GetAllData(model,model_serializer)
 
-class GetParagraphsFromArticle(GetDataFilteredByPk):
-    model = Paragraph
-    model_serializer = ParagraphsSerializer
+    def get(self,request):
+        return self.get_strategy.get(request)
+
 
