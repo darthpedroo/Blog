3579a33bbf2d44e428d8facc465e8f2a2b3f3509
darthpedroo <pedrovillarino06@gmail.com>
2024-02-27 00:23:50 -0300

    refactored the urls

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('articles/', views.GetAllArticlesView.as_view()),
-    path('paragraphs/', views.GetAllParagraphsView.as_view()),
-    path('paragraphs/<str:pk>', views.GetParagraphsFromArticle.as_view())
-    
+    #path('articles/', views.GetAllArticlesView.as_view()),
+    #path('paragraphs/', views.GetAllParagraphsView.as_view()),
+   # path('paragraph/<str:pk>', views.GetParagraphsFromArticle.as_view()),
+  #  path('test/', views.Article.as_view()),
+    path('articles/',views.AllArticlesView.as_view()),
+    path('articles/<str:pk>',views.SingleArticleView.as_view()),
+    path('paragraphs/', views.AllParagraphsView.as_view()),
+    path('paragraphs/<str:pk>', views.ParagraphsFromArticleView.as_view()),
+    path('paragraph/<str:pk>',views.SingleParagraphView.as_view()),
 ]
